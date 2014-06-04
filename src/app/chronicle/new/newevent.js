947ecf87b67175f9417cf18b01babe7185a64f75
Kara Shen <karashen@college.harvard.edu>
2014-06-04 11:18:36 -0400

    new files

---
+++ src/app/chronicle/new/newevent.js
@@ -0,0 +1,32 @@
+angular.module('chronicle.newevent', [
+  'chronicle.api',
+  'ui.router',
+  'ui.bootstrap'
+])
+
+.config(function($stateProvider) {
+  
+  $stateProvider.state('app.chronicle.new', {
+    url: '/new',
+    views: {
+      main: {
+        controller: 'NewEventCtrl',
+        templateUrl: 'chronicle/new/newevent.tpl.html',
+      }
+    }
+  });
+})
+
+.controller('NewEventCtrl', function($scope, $state, apiService) {
+  $scope.create = function(){
+    //todo: send to apiService
+    apiService.chronicle($scope.chronicle._id).newEvent({
+      title: $scope.title
+    }).then(function(event){
+      $scope.events.push(event);
+      $state.go('^');
+    });
+  };
+})
+
+;
