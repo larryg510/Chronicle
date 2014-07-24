ffd9f80c0a66c60ebf26bb62332fe9709c803b3b
larryg510 <lawrencekim@Lawrences-MacBook-Air.local>
2014-07-24 11:48:17 -0400

    notifications

---
+++ src/app/updates/updates.js
@@ -0,0 +1,43 @@
+angular.module('chronicle.updates', [
+  'chronicle.api',
+  'ui.router',
+  'ui.bootstrap'
+])
+
+.config(function($stateProvider){
+	$stateProvider.state('app.updates', {
+		url: 'updates',
+		views: {
+			main: {
+				controller: 'UpdatesCtrl',
+				templateUrl: 'updates/updates.tpl.html'
+			},
+			topnav: {
+				controller : 'UserNavCtrl',
+				templateUrl: 'user/user-nav.tpl.html'
+			}
+		},
+		resolve: {
+			updates: function($stateParams, apiService){
+				return apiService.updates();
+			}
+		}
+
+	});
+
+})
+
+.controller('UpdatesCtrl', function($scope, $state, apiService, user, updates){
+	$scope.user = user;
+	$scope.updates = updates;
+	console.log($scope.updates);
+	$scope.myupdates = updates.filter(function(update){
+		return $scope.user._id !== update.user._id && update.read.indexOf($scope.user._id) == -1;
+	});
+	console.log($scope.myupdates);
+
+})
+
+.controller('UserNavCtrl', function($scope, $state){
+	$scope.state = $state;
+});
