3ccc369f8a82889f676cde7afb1e74d0d3ccf133
larryg510 <lawrencekim@Lawrences-MacBook-Air.local>
2014-07-25 12:55:26 -0400

    unique/required stuff mongoose

--- server/models/user.js
+++ server/models/user.js
@@ -4,11 +4,11 @@ var express     = require('express')
   , Schema      = mongoose.Schema
 ;
 
-var UserSchema = new Schema({
-  name:       { type: String, index: 'text' },
-  username:   String,
-  password:   String,
-  email:      String,
+var UserSchema = new Schema({                                                                    
+  name:       { type: String, index: 'text', required : true},
+  username:   {type: String, required : true, unique : true},
+  password:   {type: String, required : true},
+  email:      {type: String, required : true},
   info:       {picture: String, description: String},
   sessionId:  String,
   chronicles: [{type: Schema.Types.ObjectId,  ref: 'Chronicle'}],
@@ -18,5 +18,23 @@ var UserSchema = new Schema({
    virtuals: true
  }
 });
+
+/*
+UserSchema.pre("save", function(next){
+  var self = this;
+  mongoose.models["User"].findOne({username : self.username}, function(err, results){
+    if(err){
+      next(err);
+    } else if(results){
+      console.warn('results', results);
+      self.invalidate("username", "username must be unique");
+      next(new Error("username must be unique"));
+    } else {
+      next();
+    }
+  });
+});
+*/
+
 
 var User = module.exports = mongoose.model('User', UserSchema);
